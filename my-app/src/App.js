a4a1d6e64a77b4dbad13b484b1eba36700b4e856
SHREYABANGAD <shreyabangad09@gmail.com>
2023-06-11 18:11:34 +0530

    intital commit

---
+++ my-app/src/App.js
@@ -0,0 +1,109 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+        <div class="container">
+           <h1>Rich Cuisine</h1>
+            <div class="navbar">
+            
+
+            <ul>
+                <li><a href="mainpage.html">HOME</a></li>
+                <li><a href="About.html">ABOUT</a></li>
+                <li><a href="menu.html">MENU</a></li> 
+                 <li><a class="register" href="REGISTER.html">REGISTER</a></li>
+            </ul>
+        </div>
+    </div>
+    <div class="about_head">
+    <div class="about_bg">
+        <div class="chef_img">
+        </div>
+      <div class="chefabout">
+        <h1>About Me</h1>
+        <h2>HI, MY NAME IS
+           SHREYA!</h2>
+        <p>And Rich Cuisine is my little corner of the internet!</p>  
+            
+          <p>  I'm the voice, author, and creator behind Rich Cuisine. What started as a casual hobby over 10 years ago in 2010 while I was working as a fourth grade teacher has now grown into a full-blown business (!!) that reaches millions of people with fun recipes each month, with content that has been featured on The Kitchn, CNN, Refinery29, Brit + Co, POPSUGAR, Huffington Post, The Everymom, PureWow, and more.
+            
+            I live in Saint Paul, MN with my husband Bjork and our dog Sage. My favorite things in life are a big plate of pad Thai, sunny days, and going to the dog park.</p>
+      </div>
+      
+    </div>
+    <div class="about_bg">
+        <div class="chef_img">
+        </div>
+      <div class="chefabout">
+        <h1>About Me</h1>
+        <h2>HI, MY NAME IS
+           PRIYA!</h2>
+        <p>And Rich Cuisine is my little corner of the internet!</p>  
+            
+          <p>  I'm the voice, author, and creator behind Rich Cuisine. What started as a casual hobby over 10 years ago in 2010 while I was working as a fourth grade teacher has now grown into a full-blown business (!!) that reaches millions of people with fun recipes each month, with content that has been featured on The Kitchn, CNN, Refinery29, Brit + Co, POPSUGAR, Huffington Post, The Everymom, PureWow, and more.
+            
+            I live in Saint Paul, MN with my husband Bjork and our dog Sage. My favorite things in life are a big plate of pad Thai, sunny days, and going to the dog park.</p>
+      </div>
+      
+    </div>
+</div>
+
+    <div>
+        <div class="footer">
+            <div class="section">
+                <h1>Rich Cuisine</h1>
+                
+                <h3>About</h3>
+                <h3>Recipe Index</h3>
+                <h3>Blog</h3>
+                <h3>Blogging Resources</h3>
+                <h3>Income Reports</h3> 
+                <h3>Sponsored Content</h3>
+                <h3>Media Mentions</h3>
+                <h3>Contact</h3>
+            </div>
+    
+            <div class="section">
+                <h1>Food & Recipes</h1>     
+                  
+                <h3>Taco Recipes</h3>    
+                <h3>Meal Prep Recipes</h3>  
+                <h3>Freezer Meals 101</h3>
+                <h3>Quick and Easy Recipes</h3>
+                <h3>Instant Pot Recipes</h3>
+                <h3>Pasta Recipes</h3>
+                <h3>Vegan Recipes</h3>
+                <h3>Soup Recipes</h3> 
+            </div>
+    
+            <div class="feedback">
+                <h1>Feedback</h1>
+    
+                <div class="feedback-box">
+                    <input class="feedback-input" type="text" name="bangbang" id=""></input>
+                    <h2 class="feedback-button">Send</h2>
+                </div>
+    
+                <div class="form-box">
+                    <h1>Rich Cuisine</h1>
+                    <div class="logo-box">
+                        <i class="fa-brands fa-instagram logos"></i>
+                        <i class="fa-brands fa-facebook logos"></i>
+                        <i class="fa-brands fa-pinterest logos"></i>
+                        <i class="fa-brands fa-twitter logos"></i>
+                    </div>
+                    <p>© 2023 Rich Cuisine. All Rights Reserved.</p>
+                
+                    
+                </div>
+    
+            </div>
+        </div>
+    </div>
+</div>
+  );
+}
+
+export default App;
